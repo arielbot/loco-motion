c07b80b5dd4d11517db513e53fdd265281742af6
Ariel Adams <ariel.talbot@iconfitness.com>
2022-01-05 10:33:56 -0700

    clean up simple air bender page and add button to link to home

--- components/SliderImg.js
+++
@@ -1,6 +0,0 @@
-export const galImage = [
-  { id: 1, image: "air_temple.png"},
-  { id: 2, image: "eastern_temple.jpeg"},
-  { id: 3, image: "northern_temple.jpeg"},
-  { id: 4, image: "southern_temple.jpeg"},
-];
--- pages/air.js
+++ pages/air.js
@@ -1,6 +1,6 @@
-import { motion } from "framer-motion"
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Air() {
   return (
@@ -8,7 +8,12 @@ export default function Air() {
       <h1 className={styles.title}>
         Air Temple
       </h1>
-      <Image src="/gallery/air_temple.png" alt="Air Temple" width={500} height={500} />
+      <div className={styles.center}>
+        <Image src="/gallery/air_temple.png" alt="Air Temple" width={440} height={250} />
+        <Link href="/">
+          <button className={`${styles.airLink} ${styles.center}`}>Take Me Home</button>
+        </Link>
+      </div>
     </main>
   )
 }
